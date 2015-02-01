d401721bf671c46c4026e957e3467ebb248e365d
安正超 <anzhengchao@gmail.com>
2015-02-01 09:48:44 +0800

    Update Http.php

--- Http.php
+++ Http.php
@@ -1,5 +1,6 @@
-<?php
+<?php namespace Overtrue\Wechat\Utils;
 
+use Exception;
 
 /**
  * HTTP Client
@@ -7,7 +8,7 @@
  * @example
  * <pre>
  * Usage:
- * 
+ *
  * Http::get($url, $params);
  * Http::post($url, $params);
  * Http::put($url, $params);
@@ -17,9 +18,9 @@
  *
  * Http::request('GET', $url, $params);
  * </pre>
- * 
- * @author  Joy chao <anzhengchao@gmail.com>
- * @version 1.0 2014-06-17
+ *
+ * @author  overtrue <anzhengchao@gmail.com>
+ * @version 1.0 2015-02-1
  */
 class Http
 {
@@ -32,75 +33,72 @@ class Http
 
 
     /**
-     * set user agent
-     *
-     * @param string $userAgent 
-     */
-    public function setUserAgent($userAgent)
-    {
-        self::$userAgent = $userAgent;
-    }
-
-    /**
      * 发起一个HTTP/HTTPS的请求
-     * 
+     *
      * @param string $method     请求类型    GET | POST...
      * @param string $url        接口的URL
      * @param array  $params     接口参数   array('content'=>'test', 'format'=>'json');
+     * @param arrat  $headers    扩展的包头信息
      * @param array  $files      图片信息
-     * @param arrat  $extheaders 扩展的包头信息
-     * 
+     *
      * @return string
      */
-    public static function request($method, $url, $params = array(), $files = [], array $extheaders = array())
+    public static function request($method, $url, array $params = array(), array $headers = array(), $files = [])
     {
-        if(!function_exists('curl_init')) exit('Need to open the curl extension');
+        if (!function_exists('curl_init')) exit('Need to open the curl extension');
+
         $method = strtoupper($method);
-        
+        $timeout = $files ? 30 : 3;
+
         $ci = curl_init();
-        
+
         curl_setopt($ci, CURLOPT_USERAGENT, self::$userAgent);
         curl_setopt($ci, CURLOPT_CONNECTTIMEOUT, 3);
-        
-        $timeout = $files ? 30 : 3;
-        
         curl_setopt($ci, CURLOPT_TIMEOUT, $timeout);
         curl_setopt($ci, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ci, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($ci, CURLOPT_SSL_VERIFYHOST, false);
         curl_setopt($ci, CURLOPT_HEADER, false);
-        
-        $headers = $extheaders;
+
+        if (!function_exists('curl_file_create')) {
+            function curl_file_create($filename, $mimetype = '', $postname = '') {
+                return "@$filename;filename="
+                    . ($postname ?: basename($filename))
+                    . ($mimetype ? ";type=$mimetype" : '');
+            }
+        }
 
         switch ($method) {
             case 'PUT':
             case 'POST':
             case 'PATCH':
-                $method != 'POST' && curl_setopt($ci, CURLOPT_CUSTOMREQUEST, $method);
-                
-                curl_setopt($ci, CURLOPT_POST, TRUE);
+                $method == 'POST' || curl_setopt($ci, CURLOPT_CUSTOMREQUEST, $method);
 
-                if (!empty($params)) {
-                    if(!empty($files)) {
-                        foreach($files as $index => $file) {
-                            $params[$index] = '@' . $file;
-                        }
-                        curl_setopt($ci, CURLOPT_POSTFIELDS, $params);
-                        $headers[] = 'Expect: ';
-                    } else {
-                        curl_setopt($ci, CURLOPT_POSTFIELDS, http_build_query($params));
+                curl_setopt($ci, CURLOPT_POST, true);
+
+                if (!empty($files)) {
+                    foreach($files as $index => $file) {
+                        $params[$index] = curl_file_create($file);
                     }
-                }
-                break;
 
-            case 'DELETE':
+                    if (phpversion() > '5.5') {
+                        curl_setopt($ci, CURLOPT_SAFE_UPLOAD, false);
+                    }
+                    curl_setopt($ci, CURLOPT_POSTFIELDS, $params);
+                    $headers[] = 'Expect: ';
+                    $headers[] = 'Content-Type: multipart/form-data';
+                } else {
+                    curl_setopt($ci, CURLOPT_POSTFIELDS, http_build_query($params));
+                }
+
+                break;
             case 'GET':
-            case 'OPTION':
-                $method != 'GET' && curl_setopt($ci, CURLOPT_CUSTOMREQUEST, $method);
-                
+            case 'HEAD':
+            case 'DELETE':
+            case 'OPTIONS':
+                $method == 'GET' || curl_setopt($ci, CURLOPT_CUSTOMREQUEST, $method);
                 if (!empty($params)) {
-                    $url = $url . (strpos($url, '?') ? '&' : '?')
-                        . (is_array($params) ? http_build_query($params) : $params);
+                    $url .= (strpos($url, '?') ? '&' : '?') . http_build_query($params);
                 }
 
                 break;
@@ -108,18 +106,33 @@ class Http
 
         curl_setopt($ci, CURLINFO_HEADER_OUT, true);
         curl_setopt($ci, CURLOPT_URL, $url);
+
         if ($headers) {
-            curl_setopt($ci, CURLOPT_HTTPHEADER, $headers );
+            curl_setopt($ci, CURLOPT_HTTPHEADER, $headers);
         }
 
         $response = curl_exec($ci);
-        curl_close ($ci);
 
+        if (curl_errno($ci)) {
+            error_log("curl错误：" . curl_errno($ci) . ' : ' . curl_error($ci));
+        }
+
+        curl_close($ci);
         return $response;
     }
-    
+
     /**
-     * static call 
+     * set user agent
+     *
+     * @param string $userAgent
+     */
+    public static function setUserAgent($userAgent)
+    {
+        self::$userAgent = $userAgent;
+    }
+
+    /**
+     * static call
      *
      * @param string $method request method.
      * @param array  $args   request params.
@@ -129,12 +142,12 @@ class Http
     public static function __callStatic($method, $args)
     {
         $method = strtoupper($method);
-        if (!in_array($method, ['GET','POST', 'DELETE', 'PUT', 'PATCH','HEAD', 'OPTION'])) {
-            throw new RuntimeException("method $method", 400);
+
+        if (!in_array($method, ['GET','POST', 'DELETE', 'PUT', 'PATCH','HEAD', 'OPTIONS'])) {
+            throw new Exception("method $method not support", 400);
         }
 
         array_unshift($args, $method);
-
         return call_user_func_array(array(__CLASS__, 'request'), $args);
     }
 }
